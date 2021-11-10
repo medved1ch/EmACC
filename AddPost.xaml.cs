952787ea802b16706da5923157b71b06035da8f9
medved1ch <rider72035@gmail.com>
2021-11-10 15:03:55 +0300

    123

--- AddPost.xaml.cs
+++ AddPost.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using System.Data.SQLite;
 using uchet.Connection;
-using System.Data;
 
 namespace uchet
 {
